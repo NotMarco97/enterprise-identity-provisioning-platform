6db0f91b70d967cd1656c4cd5c1841a22dd39064
NotMarco97 <ma.marcoadame@gmail.com>
2026-07-14 14:33:07 -0500

    Completed Employee entitiy

--- src/main/java/com/github/NotMarco97/enterprise_identity_provisioning_platform/entities/Employee.java
+++ src/main/java/com/github/NotMarco97/enterprise_identity_provisioning_platform/entities/Employee.java
@@ -1,4 +1,103 @@
 package com.github.NotMarco97.enterprise_identity_provisioning_platform.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    String firstName;
+    String lastName;
+    String status;
+    String department;
+    String jobTitle;
+    String email;
+    double salary;
+    String createdAt;
+    String updatedAt;
+    String employeeId;
+
+    public String getFirstName(){
+        return firstName;
+    }
+
+    public void setFirstName(String firstName){
+        this.firstName = firstName;
+    }
+
+    public String getLastName(){
+        return lastName;
+    }
+
+    public void setLastName(String lastName){
+        this.lastName = lastName;
+    }
+
+    public String getStatus(){
+        return status;
+    }
+
+    public void setStatus(String status){
+        this.status = status;
+    }
+
+    public String getDepartment(){
+        return department;
+    }
+
+    public void setDepartment(String department){
+        this.department = department;
+    }
+
+    public String getJobTitle(){
+        return jobTitle;
+    }
+
+    public void setJobTitle(String jobTitle){
+        this.jobTitle = jobTitle;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+
+    public void setEmail(String email){
+        this.email = email;
+    }
+
+    public double getSalary(){
+        return salary;
+    }
+
+    public void setSalary(double salary){
+        this.salary = salary;
+    }
+
+    public String getCreatedAt(){
+        return createdAt;
+    }
+
+    public void setCreatedAt(String createdAt){
+        this.createdAt = createdAt;
+    }
+
+    public String getUpdatedAt(){
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(String updatedAt){
+        this.createdAt = createdAt;
+    }
+
+    public String getEmployeeId(){
+        return employeeId;
+    }
+
+    public void setEmployeeId(String employeeId){
+        this.employeeId = employeeId;
+    }
 }
