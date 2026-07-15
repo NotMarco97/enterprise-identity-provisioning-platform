8ef6a1ecdd0e1a85d8a60aaf1da1f561ed38a720
NotMarco97 <ma.marcoadame@gmail.com>
2026-07-14 21:49:38 -0500

    Added EmployeeResponse

--- src/main/java/com/github/NotMarco97/enterprise_identity_provisioning_platform/dto/EmployeeResponse.java
+++ src/main/java/com/github/NotMarco97/enterprise_identity_provisioning_platform/dto/EmployeeResponse.java
@@ -1,4 +1,101 @@
 package com.github.NotMarco97.enterprise_identity_provisioning_platform.dto;
 
 public class EmployeeResponse {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String status;
+    private String department;
+    private String jobTitle;
+    private String email;
+    private double salary;
+    private String createdAt;
+    private String updatedAt;
+    private String employeeId;
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getFirstName(){
+        return firstName;
+    }
+
+    public void setFirstName(String firstName){
+        this.firstName = firstName;
+    }
+
+    public String getLastName(){
+        return lastName;
+    }
+
+    public void setLastName(String lastName){
+        this.lastName = lastName;
+    }
+
+    public String getStatus(){
+        return status;
+    }
+
+    public void setStatus(String status){
+        this.status = status;
+    }
+
+    public String getDepartment(){
+        return department;
+    }
+
+    public void setDepartment(String department){
+        this.department = department;
+    }
+
+    public String getJobTitle(){
+        return jobTitle;
+    }
+
+    public void setJobTitle(String jobTitle){
+        this.jobTitle = jobTitle;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+
+    public void setEmail(String email){
+        this.email = email;
+    }
+
+    public double getSalary(){
+        return salary;
+    }
+
+    public void setSalary(double salary){
+        this.salary = salary;
+    }
+
+    public String getCreatedAt(){
+        return createdAt;
+    }
+
+    public void setCreatedAt(String createdAt){
+        this.createdAt = createdAt;
+    }
+
+    public String getUpdatedAt(){
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(String updatedAt){
+        this.createdAt = createdAt;
+    }
+
+    public String getEmployeeId(){
+        return employeeId;
+    }
+
+    public void setEmployeeId(String employeeId){
+        this.employeeId = employeeId;
+    }
 }
