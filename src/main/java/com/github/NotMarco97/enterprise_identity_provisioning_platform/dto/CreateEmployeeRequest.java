3370b615d762375c9c37c877fd1811f008d6811f
NotMarco97 <ma.marcoadame@gmail.com>
2026-07-14 20:36:48 -0500

    Add CreateEmployeeRequest

--- src/main/java/com/github/NotMarco97/enterprise_identity_provisioning_platform/dto/CreateEmployeeRequest.java
+++ src/main/java/com/github/NotMarco97/enterprise_identity_provisioning_platform/dto/CreateEmployeeRequest.java
@@ -1,4 +1,78 @@
 package com.github.NotMarco97.enterprise_identity_provisioning_platform.dto;
 
 public class CreateEmployeeRequest {
+    private Long id;
+    private String employeeId;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String department;
+    private String jobTitle;
+    private String salary;
+
+    public Long getId(){
+        return id;
+    }
+
+    public void setId(Long id){
+        this.id = id;
+    }
+
+    public String getEmployeeId(){
+        return employeeId;
+    }
+
+    public void setEmployeeId(String employeeId){
+        this.employeeId = employeeId;
+    }
+
+    public String getFirstName(){
+        return firstName;
+    }
+
+    public void setFirstName(String firstName){
+        this.firstName = firstName;
+    }
+
+    public String getLastName(){
+        return lastName;
+    }
+
+    public void setLastName(String lastName){
+        this.lastName = lastName;
+    }
+
+
+    public String getEmail(){
+        return email;
+    }
+
+    public void setEmail(String email){
+        this.email = email;
+    }
+
+    public String getDepartment(){
+        return department;
+    }
+
+    public void setDepartment(String department){
+        this.department = department;
+    }
+
+    public String getJobTitle(){
+        return jobTitle;
+    }
+
+    public void setJobTitle(String jobTitle){
+        this.jobTitle = jobTitle;
+    }
+
+    public String getSalary(){
+        return salary;
+    }
+
+    public void setSalary(String salary){
+        this.salary = salary;
+    }
+
 }
